5bc978ce3db521cac655dfb06bc8733b05ba0bd1
义庆 石 <szsyq123@163.com>
2017-08-21 23:48:17 +0800

    添加项目文件。

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+            //return View();
+        }
+        public ViewResult AutoProperty()
+        {
+            //创建一个新的Product对象
+            Product myProduct = new Product();
+            //设置属性值
+            myProduct.Name = "Kayak";
+            //读取属性
+            string productName = myProduct.Name;
+            //生成视图
+            return View("Result", (object)String.Format("Product name:{0}", productName));
+        }
+    }
+}
